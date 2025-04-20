1025799574d7e3a685863a71cd2bd6989bb7250b
youssefkd1 <yousseffiras555@gmail.com>
2025-04-20 14:16:22 +0200

    0

--- js/main.js
+++ js/main.js
@@ -9,3 +9,6 @@ var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
   });
+
+  alert('this website has high quality 3d models so your phone may slow down or lag if you have good pc enjoy the website');
+  alert("I am good in web performance optimization but this models can not be optimized because it is a library called spline");
